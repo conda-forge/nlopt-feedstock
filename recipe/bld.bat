ccc1ba6b8dad35f756c0cf24b4f33bb385929a8b
Julien Schueller <schueller@phimeca.com>
2017-01-07 12:55:00 +0100

    Python 3.6

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,25 +1,19 @@
-@echo off
-
 @rem See https://github.com/jyypma/nloptr/blob/master/INSTALL.windows
 cp "%RECIPE_DIR%\CMakeLists.txt" .
 if errorlevel 1 exit 1
 cp "%RECIPE_DIR%\config.cmake.h.in" .
 if errorlevel 1 exit 1
 
-mkdir build
-cd build
+mkdir build && cd build
 
 set CMAKE_CONFIG="Release"
 
-cmake -LAH -G"NMake Makefiles"                               ^
-    -DPYTHON_EXECUTABLE="%PYTHON%"                           ^
-    -DPYTHON_INCLUDE_PATH="%PREFIX%\include"                 ^
-    -DPYTHON_LIBRARY="%PREFIX%\libs\python%PY_VER:.=%.lib"   ^
-    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ..
+cmake -LAH -G"NMake Makefiles"                     ^
+  -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"           ^
+  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ..
+if errorlevel 1 exit 1
 
-cmake --build . --config %CMAKE_CONFIG% --target ALL_BUILD
-cmake --build . --config %CMAKE_CONFIG% --target INSTALL
-
+cmake --build . --config %CMAKE_CONFIG% --target install
 if errorlevel 1 exit 1
 
 

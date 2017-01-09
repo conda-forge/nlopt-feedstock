db6ccb8c1820d42cd4794cff249826c7010e8b25
Julien Schueller <schueller@phimeca.com>
2017-01-09 18:59:17 +0100

    Python windows support

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,8 @@
-@rem See https://github.com/jyypma/nloptr/blob/master/INSTALL.windows
-cp "%RECIPE_DIR%\CMakeLists.txt" .
-if errorlevel 1 exit 1
-cp "%RECIPE_DIR%\config.cmake.h.in" .
-if errorlevel 1 exit 1
+
+# cmake support in git
+git clone https://github.com/stevengj/nlopt.git
+cd nlopt
+git checkout f4fc543
 
 mkdir build && cd build
 
@@ -10,7 +10,8 @@ set CMAKE_CONFIG="Release"
 
 cmake -LAH -G"NMake Makefiles"                     ^
   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"           ^
-  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ..
+  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"        ^
+  -DINSTALL_PYTHON_DIR="%SP_DIR%" ..
 if errorlevel 1 exit 1
 
 cmake --build . --config %CMAKE_CONFIG% --target install

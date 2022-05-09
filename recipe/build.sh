99dbac29d196f96532b852b51259a19581932da0
Julien Schueller <schueller@phimeca.com>
2022-05-09 15:02:15 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,6 @@ set -x
 if test -f "${PREFIX}/bin/pypy"
 then
   PYPY_ARGS="-DPYTHON_INCLUDE_DIR=${PREFIX}/include/pypy${CONDA_PY:0:1}.${CONDA_PY:1} -DPYTHON_LIBRARY=${PREFIX}/lib/libpypy${CONDA_PY:0:1}.${CONDA_PY:1}-c${SHLIB_EXT}"
-  #-DINSTALL_PYTHON_DIR=lib/pypy${CONDA_PY:0:1}.${CONDA_PY:1}/site-packages
 fi
 
 mkdir build && cd build
@@ -17,20 +16,11 @@ cmake ${CMAKE_ARGS} \
 
 make install -j${CPU_COUNT}
 
-ext_path=
-
-#if test -f "${PREFIX}/bin/pypy"
-#then
-#  ARCH=`uname -m`
-#  EXT=`${PREFIX}/bin/pypy -c "import importlib.machinery; print(importlib.machinery.EXTENSION_SUFFIXES[0])" | sed "s|x86_64|${ARCH}|g"`
-#  mv ${PREFIX}/lib/pypy${CONDA_PY:0:1}.${CONDA_PY:1}/site-packages/_nlopt.so ${PREFIX}/lib/pypy${CONDA_PY:0:1}.${CONDA_PY:1}/site-packages/_nlopt${EXT}
-#fi
-
 if test "$CONDA_BUILD_CROSS_COMPILATION" = "1"
 then
-  ext_path=`python -c "import sysconfig, os; print(sysconfig.get_path('platlib').replace(sysconfig.get_path('data'), '').lstrip(os.path.sep))"`
-  ext=`${PREFIX}/bin/python -c "import importlib.machinery; print(importlib.machinery.EXTENSION_SUFFIXES[0])"`
-  echo "cdt_arch=${cdt_arch}"
+  sp_dir=`python -c "import sysconfig, os; print(sysconfig.get_path('platlib').replace(sysconfig.get_path('data'), '').lstrip(os.path.sep))"`
+  mod_ext=`python -c "import importlib.machinery; print(importlib.machinery.EXTENSION_SUFFIXES[0])"`
+  mv ${PREFIX}/${sp_dir}/_nlopt.so ${PREFIX}/${sp_dir}/_nlopt${mod_ext}
 fi
 
 DIST_INFO_PATH=${SP_DIR}/${PKG_NAME}-${PKG_VERSION}.dist-info

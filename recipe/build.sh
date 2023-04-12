daac9b710e76a9f77260f042352ab055dc0291f9
Julien Schueller <schueller@phimeca.com>
2023-04-12 17:51:58 +0200

    debug

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,7 +12,11 @@ then
   target_arch=`echo ${HOST} | cut -f 1 -d "-"`
   test -f "${PREFIX}/bin/pypy" && target_arch=`echo ${target_arch} | sed "s|powerpc64le|ppc_64|g"`
   mod_ext=`python -c "import importlib.machinery; print(importlib.machinery.EXTENSION_SUFFIXES[0])" | sed "s|x86_64|${target_arch}|g"`
-  CROSS_ARGS="-DPYTHON_EXTENSION_MODULE_SUFFIX=${mod_ext} -DPYTHON_EXECUTABLE=${BUILD_PREFIX}/bin/python -DPYTHON_LIBRARY=${BUILD_PREFIX}/lib/libpython${CONDA_PY:0:1}.${CONDA_PY:1}${SHLIB_EXT} -DPYTHON_INCLUDE_DIR=${BUILD_PREFIX}/include/python${CONDA_PY:0:1}.${CONDA_PY:1}"
+  CROSS_ARGS="-DPYTHON_EXTENSION_MODULE_SUFFIX=${mod_ext}"
+  if ! test -f "${PREFIX}/bin/pypy"
+  then
+    CROSS_ARGS="${CROSS_ARGS} -DPYTHON_EXECUTABLE=${BUILD_PREFIX}/bin/python -DPYTHON_LIBRARY=${BUILD_PREFIX}/lib/libpython${CONDA_PY:0:1}.${CONDA_PY:1}${SHLIB_EXT} -DPYTHON_INCLUDE_DIR=${BUILD_PREFIX}/include/python${CONDA_PY:0:1}.${CONDA_PY:1}"
+  fi
 fi
 
 mkdir build && cd build
